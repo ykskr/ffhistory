64a6af3ff1dc63982b23d32d5584a5483fc96746
ykskr <im@nowhere.net>
2017-06-02 18:10:04 +0900

    create least files to show history

---
+++ sidebar/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('load',function(){
+ var root=document.getElementById('list');
+ while(root.firstChild)root.removeChild(root.firstChild);
+ var domains=new Object();
+ browser.history.search({'text':''}).then((logs)=>{
+  logs.forEach((log)=>{
+   if(!log.url.match(new RegExp('^(https?://[^/]+/|file:///)(.*)'))){return;}
+   if(!domains[RegExp.$1])domains[RegExp.$1]=new Object();
+   domains[RegExp.$1][RegExp.$2]=log;
+  });
+  Object.keys(domains).sort().forEach((domain)=>{
+   var elem=document.createElement('h2');
+   elem.appendChild(document.createTextNode(domain));
+   root.appendChild(elem);
+   var ebox=document.createElement('div');
+   Object.keys(domains[domain]).sort().forEach((path)=>{
+    var elem=document.createElement('div');
+    elem.appendChild(document.createTextNode(domains[domain][path].title));
+    elem.setAttribute('title',domains[domain][path].title+'\n'+domains[domain][path].url);
+    root.appendChild(elem);
+   });
+  });
+ });
+},false);
