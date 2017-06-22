5039826ec6f781d8d39b8260c0f93ab2f47d0c29
ykskr <im@nowhere.net>
2017-06-22 18:31:58 +0900

    modify to be clickable

--- sidebar/app.js
+++ sidebar/app.js
@@ -14,8 +14,10 @@ window.addEventListener('load',function(){
    root.appendChild(elem);
    var ebox=document.createElement('div');
    Object.keys(domains[domain]).sort().forEach((path)=>{
-    var elem=document.createElement('div');
+    var elem=document.createElement('a');
     elem.appendChild(document.createTextNode(domains[domain][path].title));
+    elem.setAttribute('href',domains[domain][path].url);
+    elem.setAttribute('target','_blank');
     elem.setAttribute('title',domains[domain][path].title+'\n'+domains[domain][path].url);
     root.appendChild(elem);
    });
